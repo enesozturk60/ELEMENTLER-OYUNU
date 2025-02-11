60a7085c2d0e9b7c08336a2b3af12eb4fae90e64
enesozturk60 <127125620+enesozturk60@users.noreply.github.com>
2025-02-11 12:42:41 +0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,22 @@
+var gamename = "ELEMENTLER OYUNU";
+
+var cards = [
+    { word: 'Hidrojen', meaning: 'H', image: ''},
+    { word: 'Helyum', meaning: 'He', image: '' },
+    { word: 'Lityum', meaning: 'Li', image: '' },
+    { word: 'Berilyum', meaning: 'Be', image: '' },
+    { word: 'Bor', meaning: 'B', image: '' },
+    { word: 'Karbon', meaning: 'C', image: '' },
+    { word: 'Azot', meaning: 'N', image: '' },
+    { word: 'Oksijen', meaning: 'O', image: '' },
+    { word: 'Flor', meaning: 'F', image: '' },
+    { word: 'Neon', meaning: 'Ne', image: '' },
+    { word: 'Sodyum', meaning: 'Na', image: '' },
+    { word: 'Magnezyum', meaning: 'Mg', image: '' },
+    { word: 'Alüminyum', meaning: 'Al', image: '' },
+    { word: 'Silisyum', meaning: 'Si', image: '' },
+    { word: 'Fosfor', meaning: 'P', image: '' },
+    { word: 'Kükürt', meaning: 'S', image: '' },
+    { word: 'Klor', meaning: 'Cl', image: '' },
+    { word: 'Argon', meaning: 'Ar', image: '' }
+];
